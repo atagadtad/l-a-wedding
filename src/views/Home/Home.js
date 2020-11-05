31d08b95067d5a0c06715c586dc3f50ae296497c
Aaron Tagadtad <aaron@alias.com>
2020-11-04 23:11:35 -0500

    added ceremony location

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,6 +8,21 @@ const Home = () => {
           {`Lelaine & Aaron Are Tying The Knot`}
         </h1>
       </div>
+      <div>
+        <h1 className="text-3xl font-semibold px-4 pt-3">Ceremony</h1>
+      </div>
+      <div>
+        <h1 className="text-gray-300 px-4 text-2xl font-semibold underline">
+          The Walper Hotel
+        </h1>
+      </div>
+      <div>
+        <p className="text-gray-300 px-4 text-xl font-semibold">
+          20 Queen St South <br />
+          Kitchener, ON <br />
+          4:00 pm
+        </p>
+      </div>
     </div>
   );
 };

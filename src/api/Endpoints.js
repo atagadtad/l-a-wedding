dc5c6e8bd0d470ef2a1684ad1f5a95f6228336c9
Aaron Tagadtad <aaron@alias.com>
2020-11-06 20:44:58 -0500

    removed unused vars

--- src/api/Endpoints.js
+++ src/api/Endpoints.js
@@ -1,4 +1,5 @@
 const LOCAL_URL = "http://127.0.0.1:8000/api";
-const LOGIN = `${LOCAL_URL}/login`;
+const LIVE_URL = "http://l-a-wedding.ca-central-1.elasticbeanstalk.com/api";
+const LOGIN = `${LIVE_URL}/login`;
 
 export { LOCAL_URL, LOGIN };

c80dabb9253101b86b2e55886a63082d852cc3a6
Aaron Tagadtad <aaron@alias.com>
2020-11-08 09:51:31 -0500

    added https endpoint for server

--- src/api/Endpoints.js
+++ src/api/Endpoints.js
@@ -1,5 +1,5 @@
 const LOCAL_URL = "http://127.0.0.1:8000/api";
-const LIVE_URL = "http://l-a-wedding.ca-central-1.elasticbeanstalk.com/api";
+const LIVE_URL = "https://server.l-a-wedding.net/api";
 const LOGIN = `${LIVE_URL}/login`;
 
 export { LOCAL_URL, LOGIN };

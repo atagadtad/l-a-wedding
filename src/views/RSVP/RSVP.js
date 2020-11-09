96f98dfdccb25e1db09c8b8bca7db0140f3d2c39
Aaron Tagadtad <aaron@alias.com>
2020-11-08 22:10:50 -0500

    added form modal on rsvp.js

--- src/views/RSVP/RSVP.js
+++ src/views/RSVP/RSVP.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RSVP = () => {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <div className="sm:container h-screen  bg-green-700">
       <div className="pt-16 h-full flex items-center">
@@ -15,12 +17,40 @@ const RSVP = () => {
             </p>
           </div>
           <div className="px-4 py-2">
-            <button className="uppercase border focus:outline-none text-lg font-thin text-white py-3 px-5 border-solid border-white">
+            <button
+              onClick={() => setShowForm(!showForm)}
+              className="uppercase border focus:outline-none text-lg font-thin text-white py-3 px-5 border-solid border-white"
+            >
               I'll be there!
             </button>
           </div>
         </div>
       </div>
+      {showForm && (
+        <div className="h-screen absolute inset-0 bg-red-300">
+          <div className="px-3 py-3 flex w-screen justify-end">
+            <button
+              className="flex items-center px-3 py-2  text-teal-200 border-teal-400 hover:text-white hover:border-white"
+              onClick={() => setShowForm(!showForm)}
+            >
+              <svg
+                className="text-white h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

d20cc4e084fa3dbb13d8a40789811f0198eaa748
Aaron Tagadtad <aaron@alias.com>
2021-01-24 18:51:56 -0500

    added line spacing to login button and changed header text to full white

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -44,7 +44,7 @@ const Login = () => {
 
   return (
     <div className="container bg-green-700 px-4">
-      <h1 className="text-gray-300 flex justify-center items-center px-4 py-6 text-6xl font-serif">
+      <h1 className="text-white flex justify-center items-center px-4 py-6 text-6xl font-serif">
         {"Login"}
       </h1>
 
@@ -64,7 +64,7 @@ const Login = () => {
       <div className="px-4 py-8 flex justify-center">
         <button
           onClick={login}
-          className="uppercase border hover:bg-gray-300 hover:text-gray-500 hover:border-gray-300 focus:outline-none text-lg font-thin font-serif text-white py-3 px-5 border-solid border-white"
+          className="uppercase border tracking-widest hover:bg-gray-300 hover:text-gray-500 hover:border-gray-300 focus:outline-none text-lg font-thin font-serif text-white py-3 px-5 border-solid border-white"
         >
           {isLoggingIn ? (
             <svg class="animate-spin h-5 w-5 mx-3" viewBox="0 0 24 24">

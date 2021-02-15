1c9fae48f28d2145c4115a1d73a69af3acde17da
Aaron Tagadtad <aaron@alias.com>
2021-02-15 16:12:14 -0500

    fixed width issue for mobile screens. have modal be full width 640px below

--- src/views/RSVP/RSVP.js
+++ src/views/RSVP/RSVP.js
@@ -50,7 +50,7 @@ const RSVPForm = ({ showForm, setShowForm }) => {
         </span>
 
         <div
-          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          class="w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline"

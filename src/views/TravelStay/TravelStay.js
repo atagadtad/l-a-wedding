3c207ec595aaf2cf949641d92cb53d5f7f330660
Aaron Tagadtad <aaron@alias.com>
2021-02-28 16:09:21 -0500

    added photo for travel and stay

--- src/views/TravelStay/TravelStay.js
+++ src/views/TravelStay/TravelStay.js
@@ -7,15 +7,25 @@ const TravelStay = () => {
         <h1 className="text-white flex justify-center items-center px-4 py-2 text-6xl font-serif">
           {"Travel & Stay"}
         </h1>
-        <div className="flex justify-center my-4">
-          <a
-            rel="noreferrer"
-            target="_blank"
-            className="underline font-serif text-xl text-white transition hover:text-gray-900"
-            href="https://www.walper.com/"
-          >
-            The Walper Hotel
-          </a>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-12">
+          <div className="flex mx-6 justify-center items-center">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              className="underline font-serif text-xl text-white transition hover:text-gray-900"
+              href="https://www.walper.com/"
+            >
+              The Walper Hotel
+            </a>
+          </div>
+          <div className="mx-6">
+            <img
+              className="object-contain rounded-md shadow-md"
+              alt="WALPER"
+              src="https://l-a-wedding.s3.ca-central-1.amazonaws.com/walper.jpg"
+            />
+          </div>
         </div>
       </div>
     </div>

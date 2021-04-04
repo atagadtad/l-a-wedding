29f95986de8def80ab42463c7b96581d15a59a64
Aaron Tagadtad <aaron@alias.com>
2021-04-04 16:46:13 -0400

    added boxed link to form on backend on navbar

--- src/App.js
+++ src/App.js
@@ -240,6 +240,16 @@ const DesktopMenu = ({ setShowMenu }) => {
           >
             <Link to="/rsvp">RSVP</Link>
           </li> */}
+          <li>
+            <a
+              className="border transition duration-500 ease-in-out hover:bg-white tracking-wide  hover:text-green-700 hover:border-white  focus:outline-none text-lg font-thin font-serif text-white py-3 px-5 border-solid border-white"
+              rel="noreferrer"
+              target="_blank"
+              href="https://server.l-a-wedding.net/form/create"
+            >
+              RSVP
+            </a>
+          </li>
         </div>
       </ul>
     </nav>
